d1df8e46252fc121a80fd4ac3f427c4df0439df8
AlexDjango <yotarona@gmail.com>
2021-01-16 19:45:15 +0500

    Создание курсов и удаление

--- courses/models.py
+++ courses/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -41,14 +41,14 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    # order = OrderField(blank=True, for_fields=['course'])
-    #
-    # class Meta:
-    #     ordering = ['order']
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
 
     def __str__(self):
-        return self.title
-        # return '{}. {}'.format(self.order, self.title)
+
+        return '{}. {}'.format(self.order, self.title)
 
 
 class Content(models.Model):
@@ -64,6 +64,10 @@ class Content(models.Model):
                                      on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
