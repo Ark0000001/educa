5e221434d31acbe41f9156225de7e4bccb032c27
AlexDjango <yotarona@gmail.com>
2021-01-17 17:52:45 +0500

    makemigation students 355

---
+++ students/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+# from django.views.decorators.cache import cache_page
+from . import views
+
+
+urlpatterns = [
+    path('register/',
+         views.StudentRegistrationView.as_view(),
+         name='student_registration'),
+]
